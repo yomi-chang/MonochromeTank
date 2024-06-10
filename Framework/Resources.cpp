5060da41faa4a646fda166b7f94ff296ea73bdc8
yomi-chang <yomi.saiga@gmail.com>
2024-06-10 20:54:22 +0900

    モデルデータをResourcesで読み込みように修正

--- Framework/Resources.cpp
+++ Framework/Resources.cpp
@@ -18,22 +18,15 @@ Resources* const Resources::GetInstance()
 // リソースをロードする
 void Resources::LoadResource()
 {
-	//// リソースディレクトリを設定する
-	//m_graphics->GetFX()->SetDirectory(L"resources\\cmo");
-	//// 「砲塔下部」モデルをロードする
-	//m_turretBottomModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_bottom3.cmo", *m_graphics->GetFX());	
-	//// 「砲塔脚」モデルをロードする
-	//m_turretFootModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_bottom_foot.cmo", *m_graphics->GetFX());
-	//// 「砲塔プレート」モデルをロードする
-	//m_turretPlateModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_plate.cmo", *m_graphics->GetFX());
-	//// 「砲塔中部」モデルをロードする
-	//m_turretMiddleModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_middle.cmo", *m_graphics->GetFX());
-	//// 「砲塔上部」モデルをロードする
-	//m_turretTopModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_top.cmo", *m_graphics->GetFX());
-	//// 「砲台」モデルをロードする
-	//m_turretFortModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_fort.cmo", *m_graphics->GetFX());
-	//// 「主砲」モデルをロードする
-	//m_turretGunModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\turret_gun.cmo", *m_graphics->GetFX());
-	//// 「砲弾」モデルをロードする
-	//m_bulletModel = DirectX::Model::CreateFromCMO(m_device, L"resources\\cmo\\bullet.cmo", *m_graphics->GetFX());
+	// リソースディレクトリを設定する
+	m_graphics->GetFX()->SetDirectory(L"Resources\\Models");
+
+	// 「車体」モデルをロードする
+	m_tankBodyModel = DirectX::Model::CreateFromCMO(m_device, L"Resources\\Models\\TankBody.cmo", *m_graphics->GetFX());
+
+	// 「砲塔」モデルをロードする
+	m_tankTurretModel = DirectX::Model::CreateFromCMO(m_device, L"Resources\\Models\\TankTurret.cmo", *m_graphics->GetFX());
+
+	// 「砲塔」モデルをロードする
+	m_tankCanonModel = DirectX::Model::CreateFromCMO(m_device, L"Resources\\Models\\TankCanon.cmo", *m_graphics->GetFX());
 }
--- Framework/Resources.h
+++ Framework/Resources.h
@@ -6,12 +6,12 @@
 class Resources
 {
 public:
-	//// 車体モデルの取得
-	//DirectX::Model* GetTankBodyModel() { return m_turretModel.get(); }
-	//// 砲塔モデルの取得
-	//DirectX::Model* GetTankTurretModel() { return m_turretBottomModel.get(); }
-	//// 砲身モデルの取得
-	//DirectX::Model* GetTankCanonModel() { return m_turretFootModel.get(); }
+	// 車体モデルの取得
+	DirectX::Model* GetTankBodyModel() { return m_tankBodyModel.get(); }
+	// 砲塔モデルの取得
+	DirectX::Model* GetTankTurretModel() { return m_tankTurretModel.get(); }
+	// 砲身モデルの取得
+	DirectX::Model* GetTankCanonModel() { return m_tankCanonModel.get(); }
 
 public:
 	Resources(Resources&&) = default;
@@ -27,15 +27,11 @@ public:
 private:
 	// コンストラクタ
 	Resources() noexcept
-		//m_turretModel{},
-		//m_turretBottomModel{},					// 砲塔下部モデル
-		//m_turretFootModel{},							// 砲塔脚モデル
-		//m_turretPlateModel{},						// 砲塔プレートモデル
-		//m_turretMiddleModel{},					// 砲塔中部モデル
-		//m_turretTopModel{},							// 砲塔上部モデル
-		//m_turretFortModel{},							// 砲台モデル
-		//m_turretGunModel{},							// 砲身モデル
-		//m_bulletModel{}									// 砲弾モデル
+		:
+		m_tank{},
+		m_tankBodyModel{},
+		m_tankTurretModel{},
+		m_tankCanonModel{}
 	{
 	}
 
@@ -47,22 +43,15 @@ private:
 	// デバイス
 	ID3D11Device* m_device = m_graphics->GetDeviceResources()->GetD3DDevice();
 
-	//// 「砲塔」モデル
-	//std::unique_ptr<DirectX::Model> m_turretModel;
-	//// 「砲塔下部」モデル
-	//std::unique_ptr<DirectX::Model> m_turretBottomModel;
-	//// 「砲塔脚」モデル
-	//std::unique_ptr<DirectX::Model> m_turretFootModel;
-	//// 「砲塔プレート」
-	//std::unique_ptr<DirectX::Model> m_turretPlateModel;
-	//// 「砲塔中部」
-	//std::unique_ptr<DirectX::Model> m_turretMiddleModel;
-	//// 「砲塔上部」モデル
-	//std::unique_ptr<DirectX::Model> m_turretTopModel;
-	//// 「砲台」モデル
-	//std::unique_ptr<DirectX::Model> m_turretFortModel;
-	//// 「砲身」モデル
-	//std::unique_ptr<DirectX::Model> m_turretGunModel;
-	//// 「砲弾」モデル
-	//std::unique_ptr<DirectX::Model> m_bulletModel;
+	// 戦車モデル
+	std::unique_ptr<DirectX::Model> m_tank;
+
+	// 車体モデル
+	std::unique_ptr<DirectX::Model> m_tankBodyModel;
+
+	// 砲塔モデル
+	std::unique_ptr<DirectX::Model> m_tankTurretModel;
+
+	// 砲身モデル
+	std::unique_ptr<DirectX::Model> m_tankCanonModel;
 };
